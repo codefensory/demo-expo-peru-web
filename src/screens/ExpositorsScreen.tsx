c38e1bb337111f55879e7fdbcdf2a1440ddcf844
Yeferson Mejias <yefer.el.teke@gmail.com>
2024-05-15 22:51:44 -0500

    chore: add expositors

--- src/screens/ExpositorsScreen.tsx
+++ src/screens/ExpositorsScreen.tsx
@@ -1,4 +1,5 @@
 import { MainLayout } from "../layouts/MainLayout"
+import expositors from "../assets/proveedores.png"
 
 export const ExpositorsScreen = ({ appType }) => {
   return (
@@ -8,6 +9,7 @@ export const ExpositorsScreen = ({ appType }) => {
           <h1 className="font-bold text-[56px]">EXPOSITORES</h1>
           <div className="w-36 h-3 rounded-xl bg-primary mt-4"></div>
         </div>
+        <img src={expositors} />
       </div>
     </MainLayout>
   )
